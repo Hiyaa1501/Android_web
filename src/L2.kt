4c3f2143246a8ef2ab204f10b51a9df4fc1c4a4a
hiyaa1501 <hiyabhakta@gmail.com>
2026-06-23 19:38:45 +0530

    L2: var and val

---
+++ src/L2.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var a = 14  //declaration of variable
+    println(a)
+    a = 28
+// a = "hi" //error
+
+    val b = 22
+//  b = 40  val cannot be reassigned
+
+    val e : Double = 10.7
+    print(e)
+}
+
+//variable identifies the value by itself that a is a integer
