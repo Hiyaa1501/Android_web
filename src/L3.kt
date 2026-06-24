40dfea1faee1448a18795e3736ab871c96079776
hiyaa1501 <hiyabhakta@gmail.com>
2026-06-24 19:53:43 +0530

    L3:operator

--- src/L3.kt
+++ src/L3.kt
@@ -1,7 +1,19 @@
 //operators >,<,>=,<=,==,!=
 fun main() {
-    val a = 15
-    val b = 20
+    val a = 15;
+    val b = 20;
     println("Sum of $a and $b is ${a+b}")
     println("a>b:${a>b}") // a<b then false prints
+
+    val l = 10;
+    val m = 20;
+    val n = 30;
+    println("l>m && n>m: ${l>m && n>m}")
+    println("!(l>m): ${!(l>m)}")
 }
+
+// && if both true
+// || if one of them is true
+// ! if false then true
+// ++a phele increment
+// a++ increments after
